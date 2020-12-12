6a6c20b5118fd4f6815c26b358065e5b416654c5
Aman Yadav <amanpdyadav@gmail.com>
2020-12-12 10:34:13 +0200

    Add body parser middleware
    Refs None

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import * as express from 'express';
 
 const app = express();
+app.use((req, res, next) => {
+    req['rawBody'] = '';
+    req.on('data', (chunk) => (req['rawBody'] += chunk));
+    req.on('end', () => {
+        req.body = JSON.parse(req['rawBody']);
+        next();
+    });
+});
 
 app.listen(8888, async () => {
     console.log(`App listening at http://localhost:8888`);
